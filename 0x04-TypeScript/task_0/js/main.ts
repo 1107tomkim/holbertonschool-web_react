9aad7e70dab1051ee3799d2164301e1702744560
1107tomkim <cryptotaekim@gmail.com>
2022-03-07 06:18:59 +0000

    Interface named student that has certain elements

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,7 +1,32 @@
 interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
 }
 
+const student1: Student = {
+    firstName: 'Tommy',
+    lastName: 'Kim',
+    age: 25,
+    location: 'Tulsa'
+}
+
+const student2: Student = {
+    firstName: 'Sehee',
+    lastName: 'Kim',
+    age: 23,
+    location: 'Mississippi'
+}
+
+const studentsList: Array<Student> = [student1, student2];
+
+const table = document.createElement('table');
+studentsList.forEach(function(student) {
+  const row = table.insertRow();
+  const firstname = row.insertCell();
+  const location = row.insertCell();
+  firstname.innerHTML = student.firstName;
+  location.innerHTML = student.location;
+  });
+document.body.appendChild(table);
